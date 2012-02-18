ce79206fad9618855cecdda9703e4486883c3101
Mark <littlegruz@gmail.com>
2012-02-18 14:57:18 +1100

    Updated to use new event handling

--- src/littlegruz/glasser/GlassBlockListener.java
+++ src/littlegruz/glasser/GlassBlockListener.java
@@ -4,19 +4,21 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockFromToEvent;
 import org.bukkit.event.block.BlockIgniteEvent;
-import org.bukkit.event.block.BlockListener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
 
-public class GlassBlockListener extends BlockListener{
+public class GlassBlockListener implements Listener{
    public static Glassification plugin;
 
    public GlassBlockListener(Glassification instance){
       plugin = instance;
    }
 
+   @EventHandler
    public void onBlockIgnite(BlockIgniteEvent event){
       Block block;
       World world;
@@ -32,7 +34,8 @@ public class GlassBlockListener extends BlockListener{
          world.dropItem(block.getLocation(), new ItemStack(Material.GLASS, 1));
       }
    }
-   
+
+   @EventHandler
    public void onBlockFromTo(BlockFromToEvent event){
       Block block;
       Location loc;
@@ -46,6 +49,7 @@ public class GlassBlockListener extends BlockListener{
       }
    }
 
+   @EventHandler
    public void onBlockPlace(BlockPlaceEvent event){
       Block block;
       World world;
--- src/littlegruz/glasser/Glassification.java
+++ src/littlegruz/glasser/Glassification.java
@@ -2,21 +2,14 @@ package littlegruz.glasser;
 
 import java.util.logging.Logger;
 
-import org.bukkit.event.Event;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Glassification extends JavaPlugin{
    Logger log = Logger.getLogger("This is MINECRAFT!");
-   private final GlassBlockListener gbl = new GlassBlockListener(this);
 
    public void onEnable(){
+      getServer().getPluginManager().registerEvents(new GlassBlockListener(this), this);
       log.info("Glassification v1.2 enabled");
-      PluginManager pm = this.getServer().getPluginManager();
-      pm.registerEvent(Event.Type.BLOCK_IGNITE, gbl, Event.Priority.Normal, this);
-      pm.registerEvent(Event.Type.BLOCK_PLACE, gbl, Event.Priority.Normal, this);
-      pm.registerEvent(Event.Type.BLOCK_FROMTO, gbl, Event.Priority.Normal, this);
-      
    }
 
    public void onDisable(){
